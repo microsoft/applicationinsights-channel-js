ccf213054d878b9745096727697fc2cefda09c3d
Mark Wolff <Mark.Wolff@microsoft.com>
2018-12-06 14:01:54 -0800

    Add instrumentationKey override option to channel (#38)
    * Add instrumentationKey override option to channel

--- Tests/Sender.tests.ts
+++ Tests/Sender.tests.ts
@@ -9,6 +9,7 @@ export class SenderTests extends TestClass {
 
     public testInitialize() {
         this._sender = new Sender();
+        this._sender.initialize({ instrumentationKey: 'iKey' }, new AppInsightsCore(), []);
     }
 
     public testCleanup() {
